6ad67e6f5d2f521eae907eca91044025e7b9381c
Nemesio Ordóñez <naom310@gmail.com>
2024-12-01 21:22:00 -0600

    End of section 8

---
+++ client-app/src/app/router/Routes.tsx
@@ -0,0 +1,20 @@
+import { createBrowserRouter, RouteObject } from "react-router-dom";
+import App from "../layout/App";
+import ActivityDashboard from "../../feature/activities/dashboard/ActivityDashboard";
+import ActivityForm from "../../feature/activities/form/ActivityForm";
+import ActivityDetails from "../../feature/activities/details/ActivityDetails";
+
+export const routes: RouteObject[] = [
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {path: 'activities', element: <ActivityDashboard />},
+            {path: 'activities/:id', element: <ActivityDetails />},
+            {path: 'createActivity', element: <ActivityForm key = 'create'  />},
+            {path: 'manage/:id', element: <ActivityForm key = 'manage'  />},
+        ]
+    }
+]
+
+export const router = createBrowserRouter(routes);
